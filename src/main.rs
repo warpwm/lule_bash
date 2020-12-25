d62cfd72f4348b4e643553938f7f39bab1c20756
Trim Bresilla <trim.bresilla@gmail.com>
2020-12-26 00:48:36 +0100

    Inintial clap/cli input support added

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,40 @@
 extern crate clap;
+extern crate pastel;
 
-use clap::{App, Arg, SubCommand};
+use clap::{App, Arg, SubCommand, AppSettings};
 
 fn main() {
-    let matches = App::new("lexo")
+    let matches = App::new("lule")
         .version("0.1")
-        .about("research paper and bibliography manager")
+        .before_help("Some info I'd like to appear before the help info")
+        .about("a command line to set 255 colors on tty's and other places that use ANSI colors")
+        .after_help("Does really amazing things to great people...but be careful with -R")
+        .global_setting(AppSettings::ColorAuto)
+        .global_setting(AppSettings::ColoredHelp)
+        .global_setting(AppSettings::DeriveDisplayOrder)
+        .global_setting(AppSettings::InferSubcommands)
+        .global_setting(AppSettings::VersionlessSubcommands)
+        .global_setting(AppSettings::AllowNegativeNumbers)
+        .global_setting(AppSettings::DontCollapseArgsInUsage)
+        .global_setting(AppSettings::DisableHelpSubcommand)
+        .setting(AppSettings::SubcommandRequiredElseHelp)
         .arg(
-            Arg::with_name("config")
-                .short("c")
-                .long("conf")
-                .value_name("FILE")
-                .help("Sets a custom config file")
+            Arg::with_name("configs")
+                .long("configs")
+                .value_name("FILEPATH")
+                .help("specify a config file where to load color preferences")
                 .takes_value(true)
         )
         .arg(
-            Arg::with_name("libraries")
-                .aliases(&["lib", "dir"])
-                .short("l")
-                .long("libs")
-                .value_name("PATH")
-                .requires("config")
-                .conflicts_with("output")
-                .help("Sets a custom path for the libraries")
-                .takes_value(true)
-        )
-        .arg(
-            Arg::with_name("database")
-                .aliases(&["data", "base"])
-                .short("d")
-                .long("data")
-                .value_name("FILE")
-                .conflicts_with("output")
-                .help("Sets a custom path for database cache")
+            Arg::with_name("template")
+                .long("templates")
+                .value_name("FILEPATH")
+                .help("specify a config file where to templates are set")
                 .takes_value(true)
         )
         .subcommand(
-            SubCommand::with_name("add")
-                .about("Adds a document into a given library")
+            SubCommand::with_name("create")
+                .about("Generate new colors from an image")
                 .arg(
                     Arg::with_name("from")
                         .help("Add document from a specific importer")
@@ -47,7 +44,6 @@ fn main() {
                         .value_name("VALUE")
                         .possible_values(&["doi", "arxiv"])
                         .requires("source")
-                        .required(true)
                 )
                 .arg(
                     Arg::with_name("source")
@@ -57,7 +53,6 @@ fn main() {
                         .takes_value(true)
                         .requires("from")
                         .value_name("SOURCE")
-                        .required(true)
                 )
                 .arg(
                     Arg::with_name("input")
@@ -66,36 +61,12 @@ fn main() {
                 ),
         )
         .subcommand(
-            SubCommand::with_name("edit")
-                .about("Edit document information from a given library")
+            SubCommand::with_name("theme")
+                .about("Switch theme form dark to light or vice-versa")
         )
         .subcommand(
-            SubCommand::with_name("browse")
-                .about("Open document's url in a browser")
-        )
-        .subcommand(
-            SubCommand::with_name("export")
-                .about("Export a document from a given library")
-        )
-        .subcommand(
-            SubCommand::with_name("move")
-                .about("Move a document into some other path")
-        )
-        .subcommand(
-            SubCommand::with_name("delete")
-                .about("Delete a document from a library")
-        )
-        .subcommand(
-            SubCommand::with_name("list")
-                .about("List to STDOUT documents of a library")
-        )
-        .subcommand(
-            SubCommand::with_name("show")
-                .about("Show all documents in a TUI environment")
-        )
-        .subcommand(
-            SubCommand::with_name("help")
-                .about("Show all documents in a TUI environment")
+            SubCommand::with_name("colors")
+                .about("Display current colors in terminal")
         )
         .get_matches();
 
